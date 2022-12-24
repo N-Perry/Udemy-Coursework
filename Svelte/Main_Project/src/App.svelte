9156773160d4f4c13bc3381e5eb0d20635212abf
Nick Perry <nperry43@gmail.com>
2022-12-23 16:33:37 -0800

    Reactivity intro

--- Svelte/Main_Project/src/App.svelte
+++ Svelte/Main_Project/src/App.svelte
@@ -2,13 +2,20 @@
   let name = "Nick";
   let age = 24;
 
+  $: uppercaseName = name.toUpperCase();
+
   function incrementAge() {
     age += 1;
+  }
+
+  function changeName() {
+    name = "Nicholas";
   }
 </script>
 
 <h1>Hello {name}, my age is {age}</h1>
 <button on:click={incrementAge}>Change Age</button>
+<button on:click={changeName}>Change Name</button>
 
 <style>
   h1 {
