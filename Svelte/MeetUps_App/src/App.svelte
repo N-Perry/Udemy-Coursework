e488066aa7faee0102896c17c50d83c82b51d53b
Nick Perry <nperry43@gmail.com>
2023-01-05 14:59:09 -0800

    created MetupItem component

--- Svelte/MeetUps_App/src/App.svelte
+++ Svelte/MeetUps_App/src/App.svelte
@@ -1,5 +1,42 @@
 <script>
   import Header from "./UI/Header.svelte";
+  import MeetupItem from "./Meetups/MeetupItem.svelte";
+
+  const meetups = [
+    {
+      id: "m1",
+      title: "Coding Bootcamp",
+      subtitle: "Learn to code in 2 hours",
+      description:
+        "In this meetup, we will have some experts that teach you how to code!",
+      imageUrl:
+        "https://imgs.search.brave.com/-cpbUIT6oXZ5dAZudcw7qSohHBRWyblA67-42JGDNds/rs:fit:597:225:1/g:ce/aHR0cHM6Ly90c2Uy/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC4x/eldrNlhVVWxWQTh5/WHJOUDh1MjNRSGFG/NCZwaWQ9QXBp",
+      address: "27 Nerd Rd, 32523 New York",
+      contactEmail: "code@test.com",
+    },
+    {
+      id: "m2",
+      title: "Swim Together",
+      subtitle: "Let's go swimming!",
+      description:
+        "We will simply swim a little bit, maybe a little butterfly stroke",
+      imageUrl:
+        "https://imgs.search.brave.com/-cpbUIT6oXZ5dAZudcw7qSohHBRWyblA67-42JGDNds/rs:fit:597:225:1/g:ce/aHR0cHM6Ly90c2Uy/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC4x/eldrNlhVVWxWQTh5/WHJOUDh1MjNRSGFG/NCZwaWQ9QXBp",
+      address: "33 Water Ln, 44444 Maine",
+      contactEmail: "swim@test.com",
+    },
+  ];
 </script>
 
 <Header />
+<section id="meetups">
+  {#each meetups as meetup}
+    <MeetupItem />
+  {/each}
+</section>
+
+<style>
+  #meetups {
+    margin-top: 5rem;
+  }
+</style>
