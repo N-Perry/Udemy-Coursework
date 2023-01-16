140bdcec4719ae3f7cbf3bb1f23de4f9b54e8f16
Nick Perry <nperry43@gmail.com>
2023-01-16 13:55:11 -0800

    started learning about custom events, allowing us to pass data up from component->parent

---
+++ Svelte/Section_Work/Section_3/src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+  import Product from "./Product.svelte";
+
+  function appAddToCart(event) {
+    console.log(event);
+  }
+
+  function appDeleteProduct(event) {
+    console.log(event);
+  }
+</script>
+
+<Product
+  productTitle="A Book..."
+  on:add-to-cart={appAddToCart}
+  on:delete={appDeleteProduct}
+/>
+
+<style>
+  h1 {
+    color: purple;
+  }
+</style>
