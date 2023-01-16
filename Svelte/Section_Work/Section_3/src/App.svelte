8e7fe52f220bd022b92dc68246bdc7acaa335b92
Nick Perry <nperry43@gmail.com>
2023-01-16 14:13:39 -0800

    learned about using the spread operator with props

--- Svelte/Section_Work/Section_3/src/App.svelte
+++ Svelte/Section_Work/Section_3/src/App.svelte
@@ -1,6 +1,15 @@
 <script>
   import Product from "./Product.svelte";
 
+  let products = [
+    {
+      id: "p1",
+      title: "A Book",
+      price: 9.99,
+      bestSeller: true,
+    },
+  ];
+
   function appAddToCart(event) {
     console.log(event);
   }
@@ -10,11 +19,13 @@
   }
 </script>
 
-<Product
-  productTitle="A Book..."
-  on:add-to-cart={appAddToCart}
-  on:delete={appDeleteProduct}
-/>
+{#each products as product}
+  <Product
+    {...product}
+    on:add-to-cart={appAddToCart}
+    on:delete={appDeleteProduct}
+  />
+{/each}
 
 <style>
   h1 {
