cd2e9f2d2f451c4e29854cdc99fed9219f49b2eb
Nick Perry <nperry43@gmail.com>
2023-01-26 14:54:53 -0800

    moved addMeetup & toggleFavorite out of App component and moved them into custom store

--- Svelte/MeetUps_App/src/App.svelte
+++ Svelte/MeetUps_App/src/App.svelte
@@ -5,32 +5,7 @@
   import Button from "./UI/Button.svelte";
   import Badge from "./UI/Badge.svelte";
 
-  let meetups = [
-    {
-      id: "m1",
-      title: "Coding Bootcamp",
-      subtitle: "Learn to code in 2 hours",
-      description:
-        "In this meetup, we will have some experts that teach you how to code!",
-      imageUrl:
-        "https://imgs.search.brave.com/-cpbUIT6oXZ5dAZudcw7qSohHBRWyblA67-42JGDNds/rs:fit:597:225:1/g:ce/aHR0cHM6Ly90c2Uy/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC4x/eldrNlhVVWxWQTh5/WHJOUDh1MjNRSGFG/NCZwaWQ9QXBp",
-      address: "27 Nerd Rd, 32523 New York",
-      contactEmail: "code@test.com",
-      isFavorite: false,
-    },
-    {
-      id: "m2",
-      title: "Swim Together",
-      subtitle: "Let's go swimming!",
-      description:
-        "We will simply swim a little bit, maybe a little butterfly stroke",
-      imageUrl:
-        "https://imgs.search.brave.com/-cpbUIT6oXZ5dAZudcw7qSohHBRWyblA67-42JGDNds/rs:fit:597:225:1/g:ce/aHR0cHM6Ly90c2Uy/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC4x/eldrNlhVVWxWQTh5/WHJOUDh1MjNRSGFG/NCZwaWQ9QXBp",
-      address: "33 Water Ln, 44444 Maine",
-      contactEmail: "swim@test.com",
-      isFavorite: false,
-    },
-  ];
+  import meetups from "./Meetups/meetups-store";
 
   let editMode = null;
 
@@ -38,8 +13,7 @@
     const { title, subtitle, description, imageUrl, address, contactEmail } =
       event.detail;
 
-    const newMeetup = {
-      id: Math.random().toString,
+    const meetupData = {
       title,
       subtitle,
       description,
@@ -48,18 +22,13 @@
       contactEmail,
     };
 
-    meetups = [newMeetup, ...meetups];
+    meetups.addMeetup(meetupData);
     editMode = null;
   }
 
   function toggleFavorite(event) {
     const id = event.detail;
-    const updatedMeetup = { ...meetups.find((m) => m.id === id) };
-    updatedMeetup.isFavorite = !updatedMeetup.isFavorite;
-    const index = meetups.findIndex((m) => m.id === id);
-    const updatedMeetups = [...meetups];
-    updatedMeetups[index] = updatedMeetup;
-    meetups = updatedMeetups;
+    meetups.toggleFavorite(id);
   }
 
   function cancelEdit() {
@@ -75,7 +44,7 @@
   {#if editMode === "add"}
     <EditMeetup on:save={addMeetup} on:cancel={cancelEdit} />
   {/if}
-  <MeetupGrid {meetups} on:togglefavorite={toggleFavorite} />
+  <MeetupGrid meetups={$meetups} on:togglefavorite={toggleFavorite} />
 </main>
 
 <style>
---
+++ Svelte/MeetUps_App/src/Meetups/meetups-store.js
@@ -0,0 +1,53 @@
+import { writable } from "svelte/store";
+
+const meetups = writable([
+  {
+    id: "m1",
+    title: "Coding Bootcamp",
+    subtitle: "Learn to code in 2 hours",
+    description:
+      "In this meetup, we will have some experts that teach you how to code!",
+    imageUrl:
+      "https://imgs.search.brave.com/-cpbUIT6oXZ5dAZudcw7qSohHBRWyblA67-42JGDNds/rs:fit:597:225:1/g:ce/aHR0cHM6Ly90c2Uy/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC4x/eldrNlhVVWxWQTh5/WHJOUDh1MjNRSGFG/NCZwaWQ9QXBp",
+    address: "27 Nerd Rd, 32523 New York",
+    contactEmail: "code@test.com",
+    isFavorite: false,
+  },
+  {
+    id: "m2",
+    title: "Swim Together",
+    subtitle: "Let's go swimming!",
+    description:
+      "We will simply swim a little bit, maybe a little butterfly stroke",
+    imageUrl:
+      "https://imgs.search.brave.com/-cpbUIT6oXZ5dAZudcw7qSohHBRWyblA67-42JGDNds/rs:fit:597:225:1/g:ce/aHR0cHM6Ly90c2Uy/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC4x/eldrNlhVVWxWQTh5/WHJOUDh1MjNRSGFG/NCZwaWQ9QXBp",
+    address: "33 Water Ln, 44444 Maine",
+    contactEmail: "swim@test.com",
+    isFavorite: false,
+  },
+]);
+
+const customMeetups = {
+  subscribe: meetups.subscribe,
+  addMeetup: (meetupData) => {
+    const newMeetup = {
+      ...meetupData,
+      id: Math.random().toString(),
+      isFavorite: false,
+    };
+    meetups.update((existingItems) => {
+      return [newMeetup, ...existingItems];
+    });
+  },
+  toggleFavorite: (id) => {
+    meetups.update((existingItems) => {
+      const updatedMeetup = { ...existingItems.find((m) => m.id === id) };
+      updatedMeetup.isFavorite = !updatedMeetup.isFavorite;
+      const index = existingItems.findIndex((m) => m.id === id);
+      const updatedMeetups = [...existingItems];
+      updatedMeetups[index] = updatedMeetup;
+      return updatedMeetups;
+    });
+  },
+};
+export default customMeetups;
